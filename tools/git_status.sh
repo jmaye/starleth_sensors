9f49454e96398f10a02d30bc875434ae0e228558
Jerome Maye <jerome.maye@mavt.ethz.ch>
2014-09-15 13:55:53 +0200

    new instructions for init

--- tools/git_status.sh
+++ tools/git_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FOLDERS="artor-basics aslam_visual_inertial asrl_navigation catkin_simple c_xsens_mti eigen_catkin ethzasl_icp_mapping fovis leica_totalstation_interface libfovis libvisensor_devel Schweizer-Messer starleth_sensors starleth_tr vcharge_motion_planning velodyne-post-ros velodyne_ros visensor_node_devel visensor_tools viso2"
+FOLDERS="artor-basics aslam_visual_inertial asrl_navigation catkin_simple c_xsens_mti eigen_catkin ethzasl_icp_mapping fovis leica_totalstation_interface libfovis libvisensor_devel Schweizer-Messer starleth-diagnostics-ros starleth-init starleth_sensors starleth-system-monitor-ros starleth_tr starleth-udev vcharge_motion_planning velodyne-post-ros velodyne_ros visensor_node_devel visensor_tools viso2"
 
 for FOLDER in ${FOLDERS}; do
   echo "Updating: ${FOLDER}"
